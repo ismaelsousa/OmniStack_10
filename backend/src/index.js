ca83cd2df03a031850714a7c243b2a6313e4b6e2
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-01-16 08:06:36 -0300

    FrontEnd finalizado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const mongoose = require("mongoose")
-
+const cors = require('cors')
 const routes = require('./routes')
+const app = express() 
 mongoose.connect('mongodb+srv://ismael:senha@nodecluster-imcru.mongodb.net/week10?retryWrites=true&w=majority',{ 
   useUnifiedTopology: true,
   useNewUrlParser: true 
 })
-const app = express() 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
